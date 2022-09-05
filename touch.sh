44634425323ce547fef8a5b33e7cc80755ce4829
Yashwanthkistipati <reddy.k2123@gmail.com>
2022-09-05 09:59:03 +0530

    yeshwanth1stupload

---
+++ touch.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/Yashwanthkistipati/world-population.git
+git remote -v  
+git add . --force
+git commit -m "yeshwanth1stupload"
+
+
+git push -f origin main
+
